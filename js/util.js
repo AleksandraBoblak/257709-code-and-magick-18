ceda671e6bd5810b115e7624f5b97b190c698181
Aleksandra Boblak <boblak.aleksandra@gmail.com>
2019-10-06 13:37:11 +0300

    Исправила window.util

--- js/util.js
+++ js/util.js
@@ -4,24 +4,30 @@
   var ENTER_KEYCODE = 13;
   var HIDDEN_CLASS = 'hidden';
 
-  window.util = (function () {
-    return {
-      isEscEvent: function (evt, action) {
-        if (evt.keyCode === ESC_KEYCODE) {
-          action();
-        }
-      },
-      isEnterEvent: function (evt, action) {
-        if (evt.keyCode === ENTER_KEYCODE) {
-          action();
-        }
-      },
-      hide: function (element) {
-        element.classList.add(HIDDEN_CLASS);
-      },
-      show: function (element) {
-        element.classList.remove(HIDDEN_CLASS);
-      },
-    };
-  })();
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  };
+
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+
+  var hide = function (element) {
+    element.classList.add(HIDDEN_CLASS);
+  };
+
+  var show = function (element) {
+    element.classList.remove(HIDDEN_CLASS);
+  };
+
+  window.util = {
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent,
+    hide: hide,
+    show: show
+  };
 })();
